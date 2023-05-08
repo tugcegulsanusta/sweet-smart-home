72423a4303b566c58da6ec765ad57c169a9e7df0
Tuğçe Usta <tugce.gulsan@gmail.com>
2023-05-08 11:36:35 +0300

    add StatusBox

--- src/ui/AbstractDevicePanel.java
+++ src/ui/AbstractDevicePanel.java
@@ -5,10 +5,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import model.enums.DeviceState;
 import model.interfaces.StatefulInterface;
 
 public abstract class AbstractDevicePanel extends JPanel {
@@ -18,13 +20,16 @@ public abstract class AbstractDevicePanel extends JPanel {
 	protected final JButton deleteButton;
 	private JTextField textField;
 	protected final JLabel statusLabel;
+	private JComboBox statusBox;
 
 	public AbstractDevicePanel(SmartHomeMainFrame mainFrame, StatefulInterface statefulInterface) {
 		this.statefulInterface = statefulInterface;
 		this.mainFrame = mainFrame;
 
-		statusLabel = new JLabel("Status: " + getCurrentState());
+		statusLabel = new JLabel();
+
 		statusLabel.setForeground(new Color(64, 0, 128));
+		refreshStatus();
 
 		deleteButton = new JButton("Delete");
 		deleteButton.addActionListener(new ActionListener() {
@@ -33,12 +38,36 @@ public abstract class AbstractDevicePanel extends JPanel {
 				mainFrame.removePanel(AbstractDevicePanel.this);
 			}
 		});
-		deleteButton.setBounds(201, 5, 89, 23);
+		deleteButton.setBounds(201, 15, 89, 23);
 		add(deleteButton);
 
 		statusLabel.setBounds(201, 50, 150, 50);
 		add(statusLabel);
 
+		// array of string containing cities
+
+		Object[] statuses = statefulInterface.getAvailableStates().stream().toArray();
+
+		// create checkbox
+		statusBox = new JComboBox(statuses);
+		statusBox.setBounds(201, 100, 150, 30);
+		statusBox.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				DeviceState newState = (DeviceState) statusBox.getSelectedItem();
+				statefulInterface.setCurrentState(newState);
+				refreshStatus();
+				AbstractDevicePanel.this.revalidate();
+
+			}
+		});
+		add(statusBox);
+
+	}
+
+	private void refreshStatus() {
+		statusLabel.setText("Status: " + getCurrentState());
 	}
 
 	protected String getCurrentState() {
